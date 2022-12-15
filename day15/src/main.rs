f66b65d78598d409c0e3db9d6ff8d46f6f48c072
Amidamaru <thaodt@users.noreply.github.com>
2022-12-15 16:05:40 +0700

    day 15

---
+++ day15/src/main.rs
@@ -0,0 +1,96 @@
+use helpers::*;
+use std::collections::{BTreeMap, HashSet};
+
+fn load_input() -> String {
+    std::fs::read_to_string("input").unwrap()
+}
+
+fn main() {
+    let input = load_input();
+    println!("part 1 {}", part1(&input));
+    println!("part 2 {}", part2(&input));
+}
+
+pub fn part1(input: &str) -> impl std::fmt::Display {
+    let yy = 2000000;
+    let mut ybeacons = HashSet::new();
+    let mut sensors = Vec::new();
+    let mut beacons = HashSet::new();
+    for line in input.lines() {
+        let wtf = line.split(['=', ',', ':']).collectvec();
+        let sx = wtf[1].parse::<i64>().unwrap();
+        let sy = wtf[3].parse::<i64>().unwrap();
+        let bx = wtf[5].parse::<i64>().unwrap();
+        let by = wtf[7].parse::<i64>().unwrap();
+
+        if by == yy {
+            ybeacons.insert(bx);
+        }
+
+        let r = (sx - bx).abs() + (sy - by).abs();
+        sensors.push((sx, sy, r));
+        beacons.insert((bx, by));
+    }
+
+    let mut ans = 0;
+    let mut iv: BTreeMap<i64, i64> = BTreeMap::new();
+    for &(x, y, r) in &sensors {
+        let d = r - (y - yy).abs();
+        if d < 0 {
+            continue;
+        }
+        *iv.entry(x - d).or_default() += 1;
+        *iv.entry(x + d + 1).or_default() -= 1;
+    }
+    let iv = iv.into_iter().collectvec();
+    let mut c = iv[0].1;
+    for w in iv.windows(2) {
+        if c > 0 {
+            ans += w[1].0 - w[0].0;
+        }
+        c += w[1].1;
+    }
+
+    ans - ybeacons.len() as i64
+}
+
+pub fn part2(input: &str) -> impl std::fmt::Display {
+    let mut sensors = Vec::new();
+    let mut beacons = HashSet::new();
+    for line in input.lines() {
+        let wtf = line.split(['=', ',', ':']).collectvec();
+        let sx = wtf[1].parse::<i64>().unwrap();
+        let sy = wtf[3].parse::<i64>().unwrap();
+        let bx = wtf[5].parse::<i64>().unwrap();
+        let by = wtf[7].parse::<i64>().unwrap();
+
+        let r = (sx - bx).abs() + (sy - by).abs();
+        sensors.push((sx, sy, r));
+        beacons.insert((bx, by));
+    }
+
+    for yy in 0..=4000000 {
+        let mut iv: BTreeMap<i64, i64> = BTreeMap::new();
+        for &(x, y, r) in &sensors {
+            let d = r - (y - yy).abs();
+            if d < 0 {
+                continue;
+            }
+            *iv.entry(x - d).or_default() += 1;
+            *iv.entry(x + d + 1).or_default() -= 1;
+        }
+        let mut c = 0;
+        let mut lk = -1;
+        for (&k, &v) in &iv {
+            c += v;
+            if c == 0 && lk == -1 {
+                lk = k;
+            }
+        }
+        if lk != *iv.iter().last().unwrap().0 {
+            return lk * 4000000 + yy;
+        }
+    }
+
+    0
+}
